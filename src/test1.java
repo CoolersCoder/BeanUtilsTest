5fd31c3481cbd9434f5ff809f2420232fa070be3
Rui Hu <huruidedd@gmail.com>
2016-02-27 13:31:34 -0500

    kk

---
+++ src/test1.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.junit.Test;
+
+public class test1 {
+
+	@Test
+	public void ss() throws IllegalAccessException, InvocationTargetException{
+		user us = new user();
+		
+		BeanUtils.copyProperty(us, "name", "jakc");
+		
+		System.out.println(us.getName());
+	}
+}
---
+++ src/user.java
@@ -0,0 +1,26 @@
+
+public class user {
+	int age;
+	String nice_name;
+	String name;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getNice_name() {
+		return nice_name;
+	}
+	public void setNice_name(String nice_name) {
+		this.nice_name = nice_name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
